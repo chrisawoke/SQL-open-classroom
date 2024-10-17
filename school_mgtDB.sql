3c5ad08cc59f80ead6d58bebf83160c7b8776c56
Chris Awoke <uchechristian27@gmail.com>
2024-10-17 22:09:27 +0100

    Create school_mgtDB.sql

---
+++ school_mgtDB.sql
@@ -0,0 +1,37 @@
+-- Create Database for a school
+DROP DATABASE IF EXISTS school_mgtDB;
+CREATE DATABASE school_mgtDB;
+USE school_mgtDB;
+
+-- Create the relevant tables
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+    student_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    dob DATE,
+    gender ENUM('M', 'F'),
+    enrollment_date DATE,
+    enrollment_status ENUM('Enrolled', 'Pending', 'Completed') DEFAULT 'Pending',
+    grade_level INT
+);
+
+CREATE TABLE teachers (
+    teacher_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(35) NOT NULL,
+    last_name VARCHAR(35) NOT NULL,
+    hire_date DATE NOT NULL,
+    terminate_date DATE,
+    department_id INT,
+    salary DECIMAL(10 , 2 ) CHECK (salary >= 0),
+    FOREIGN KEY (department_id)
+        REFERENCES departments (department_id)
+);
+
+CREATE TABLE departments (
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(150)
+);
+
+
+
