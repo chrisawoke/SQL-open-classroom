a8d18ccef75af529b52bf6cb68f77ee283b742d4
Chris Awoke <uchechristian27@gmail.com>
2024-10-17 22:23:30 +0100

    Update school_mgtDB.sql

--- school_mgtDB.sql
+++ school_mgtDB.sql
@@ -4,6 +4,12 @@ CREATE DATABASE school_mgtDB;
 USE school_mgtDB;
 
 -- Create the relevant tables
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(150)
+);
+
 DROP TABLE IF EXISTS students;
 CREATE TABLE students (
     student_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -16,6 +22,7 @@ CREATE TABLE students (
     grade_level INT
 );
 
+DROP TABLE IF EXISTS teachers;
 CREATE TABLE teachers (
     teacher_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(35) NOT NULL,
@@ -23,23 +30,17 @@ CREATE TABLE teachers (
     hire_date DATE NOT NULL,
     terminate_date DATE,
     department_id INT,
-    salary DECIMAL(10 , 2 ) CHECK (salary >= 0),
+    salary DECIMAL(10, 2) CHECK (salary >= 0),
     FOREIGN KEY (department_id)
-        REFERENCES departments (department_id)
+        REFERENCES departments(department_id)
 );
 
-CREATE TABLE departments (
-    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(150)
-);
-
+DROP TABLE IF EXISTS courses;
 CREATE TABLE courses (
     course_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     course_name VARCHAR(100),
     department_id INT,
     credits INT,
     FOREIGN KEY (department_id)
-        REFERENCES departments (department_id)
+        REFERENCES departments(department_id)
 );
-
-
